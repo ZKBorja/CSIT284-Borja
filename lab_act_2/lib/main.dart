fef8f1bb2ccc7fe00dd25ae0b9a0677d9811adc3
LAB12_USER ON WS33 <L12X19W33@LabHCI.net>
2026-08-19 20:01:53 +0800

    Add Gradient

--- lab_act_2/lib/main.dart
+++ lab_act_2/lib/main.dart
@@ -1,5 +1,24 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Text('Hello world')));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+                Colors.red,
+                Colors.yellow
+              ])
+            ),
+          child: Center(
+            child: Text(
+              'Hello world')),
+        )
+       )
+      )
+    );
 }
